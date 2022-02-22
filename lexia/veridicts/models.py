c980f78ba4084164e4a51839f17ba140fb1cd426
Anton Baric <anton.baric99@gmail.com>
2022-02-22 23:43:30 +0100

    Added fileField in model

--- lexia/veridicts/models.py
+++ lexia/veridicts/models.py
@@ -1,8 +1,10 @@
+import datetime
 from django.db import models
 
 # Create your models here.
 
 class Veridict(models.Model):
   court_name = models.CharField(max_length=100)
-  veridict_date = models.DateField
+  veridict_date = models.DateField(default=datetime.date.today)
   upload_date = models.DateTimeField(auto_now_add=True)
+  pdf_file = models.FileField(default=None)
